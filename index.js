dcb678fd881ecc9d56ae9a4d7f3f3bd223a34aa8
SADAF SOHAIL <159582784+sadafrao@users.noreply.github.com>
2024-04-15 15:25:20 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+// Q86
+//this function is checking text presence by using .includes()
+function includetext(str) {
+    return str.includes("sadaf");
+}
+console.log(includetext("hi i am sadaf a web developer"));
+console.log(includetext("hi i am friends a web developer"));
